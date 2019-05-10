cdd9695f18dccec2ddc7312907898716caa6a70e
medlkamel <mohamed98lkamel@gmail.com>
2019-05-10 19:25:09 +0000

    Task : adding alert-success in valide page

---
+++ user.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+        <title>Home Page</title>
+        <link href="https://fonts.googleapis.com/css?family=Patua+One" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+        <link href='assets/css/rotating-card.css' rel='stylesheet' />
+	</head>
+<body>
+		<header>
+	   		<nav class="navbar fixed-top navbar-expand navbar-light bg-light">
+	   			<a class="navbar-brand logo" href="index.html">
+	   				<img src="assets/img/logo.PNG">
+	   			</a>
+		    </nav>
+		</header>
+		
+		
+<div class="container-fluid text-center">    
+  <div class="row content" style="margin: 150px 30px 200px 0px;">
+    <div class="col-sm-2 sidenav">
+
+        <img class="img-circle" src="assets/img/user.png" width="200" height="200" />
+
+    </div>
+    <div class="col-sm-8 text-left"> 
+      <table class="table" style="margin-top:30px; " >
+    <tbody>
+      <tr>
+        
+        <td>CIN</td>
+        <td>Anna</td>
+        <td></td>
+
+      </tr>
+    <tr>
+
+        <td>Name</td>
+        <td>Anna</td>
+         <td><button type="button" class="btn btn-light btn-sm">Modify</button></td>
+ 
+      </tr>
+      <tr>
+
+        <td>Last Name</td>
+        <td>Anna</td>
+         <td><button type="button" class="btn btn-light btn-sm">Modify</button></td>
+
+      </tr>
+            <tr>
+
+        <td>Adresse</td>
+        <td>Anna</td>
+         <td><button type="button" class="btn btn-light btn-sm">Modify</button></td>
+
+      </tr>
+      <tr>
+
+        <td>Phone</td>
+        <td>Anna</td>
+         <td><button type="button" class="btn btn-light btn-sm">Modify</button></td>
+
+      </tr>
+      <tr>
+
+        <td>PassWord</td>
+        <td>Anna</td>
+         <td><button type="button" class="btn btn-light btn-sm">Modify</button></td>
+
+      </tr>
+      <tr>
+
+        <td>Birth Date</td>
+        <td>Anna</td>
+         <td><button type="button" class="btn btn-light btn-sm">Modify</button></td>
+
+      </tr>
+
+    </tbody>
+  </table>
+    </div>
+    <div class="card-body row">
+    <div class="col-sm-12 sidenav">
+    <div class="card-container">
+    <div class="card">
+    	<div class="front"> <img style="margin: 0px 0px 0px -15px;" src="assets/img/cursor.png" width="250" height="350"></div>
+    	<div class="back">
+      <div class="well">
+        <p>ADS</p>
+      </div>
+      <div class="well">
+        <p>ADS</p>
+      </div>
+    </div>
+</div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+
+<footer class="container-fluid text-center">
+  <p>Footer Text</p>
+</footer>
+</body>
+</html>
---
+++ valider.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+			<meta charset="utf-8">
+        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+        <title>Home Page</title>
+        <link href="https://fonts.googleapis.com/css?family=Patua+One" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+        <link href='assets/css/rotating-card.css' rel='stylesheet' />
+	<title>Valider</title>
+</head>
+<body>
+	<div style="margin: 200px 0px 0px 0px;">
+<div class="alert alert-success">
+  <img src="assets/img/Sin-título-1.png" width="100" height="100"><strong>Success!</strong> Indicates a successful or positive action.
+</div>
+</div>
+</body>
+</html>
